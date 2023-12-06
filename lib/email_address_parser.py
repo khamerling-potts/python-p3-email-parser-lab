c00f30ac178e2dfd11b7fec0be5d5720dcf51b43
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2023-12-06 14:29:25 -0500

    done

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,16 @@
 # your code goes here!
+import re
+
+
+class EmailAddressParser:
+    def __init__(self, addresses):
+        self.addresses = addresses
+
+    def parse(self):
+        email_regex = re.compile(r"[A-z]\.?([A-z\d]+\.?)*@[A-z\d]+\.[A-z\d]+")
+        array = re.split(r",| ", self.addresses)
+        parsed_set = set()
+        for email in array:
+            if email_regex.fullmatch(email):
+                parsed_set.add(email)
+        return sorted(list(parsed_set))
